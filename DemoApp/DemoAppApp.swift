f8160aafab47beabe027358b10da90a0d6654f6f
jcante11 <juan_cante@uri.edu>
2024-05-04 15:08:03 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Juan Cante Jr. on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
